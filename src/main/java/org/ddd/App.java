553da20b6eedcbe997598b1afbb2047fb48ebdfd
DenWorker <whiteskyD@yandex.ru>
2023-04-23 15:27:26 +0300

    Create fields and methods of classes

--- src/main/java/org/ddd/App.java
+++ src/main/java/org/ddd/App.java
@@ -1,13 +1,39 @@
 package org.ddd;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import Models.Principal;
+import Models.School;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class App {
+    static Configuration configuration = new Configuration()
+            .addAnnotatedClass(Principal.class)
+            .addAnnotatedClass(School.class);
+
+    static SessionFactory sessionFactory = configuration.buildSessionFactory();
+    static Session session = sessionFactory.getCurrentSession();
+
+    public static void main(String[] args) {
+        try {
+            session.beginTransaction();
+
+            getPrincipalAndHisSchool(6);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+
+    public static void getPrincipalAndHisSchool(int idOfPrincipal) {
+        Principal principal = session.get(Principal.class, idOfPrincipal);
+        System.out.println("Информация о директоре:");
+        System.out.println(principal.toString());
+        System.out.println("Информация о его школе директоре:");
+        System.out.println(principal.getSchool().toString());
+        System.out.println("-------------------------------------");
     }
 }
+
